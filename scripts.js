578b2682445b68fbb3d4f51335f5059603e16575
DLamarG <demondg8@gmail.com>
2024-10-14 20:10:36 -0500

    changed js file: added new function features

--- scripts.js
+++ scripts.js
@@ -20,9 +20,11 @@ document.addEventListener('DOMContentLoaded', function() {
             location: 'Flossmoor, IL, USA',
             description: `
                 <ul>
-                    <li><strong>System Hardening:</strong> Periodically update and install patches with Windows Update for Windows systems and apt for Ubuntu/Debian systems.</li>
+                    <li><strong>System Hardening:</strong> Update and install patches with Windows Update for Windows systems and apt for Ubuntu/Debian systems.</li>
                     <li><strong>Network Security:</strong> Configure pfSense firewalls, establish network rules with iptables, configure SecurityOnion, and perform pcap analysis with Wireshark to safeguard networks and attached assets.</li>
                     <li><strong>Cloud Services:</strong> Implement AWS services such as CloudWatch, Route 53, and S3 buckets to manage, monitor, and secure scalable application solutions.</li>
+                    <li><strong>Data Migration:</strong> Executed the migration of databases and file storage to cloud solutions leveraging AWS-S3, optimizing data management processes and ensuring data integrity and accessibility during the transition.</li>
+                    <li><strong>Cloud Security Monotoring:</strong> Implemented monitoring solutions by levaraging AWS CloudWatch, CloudFront, and AWS Sheild to track application performance and resource utilization, enabling proactive adjustments and tuning for optimal operational efficiency.</li>
                 </ul>
             `
         },
@@ -33,7 +35,7 @@ document.addEventListener('DOMContentLoaded', function() {
             location: 'Vernon Hills, IL, USA',
             description: `
                 <ul>
-                    <li><strong>Strategic Innovation:</strong> Led initiatives to foster innovation across Industrial platforms, aligning with corporate strategies and stakeholder needs for strategic ideation and commercialization, utilizing the latest communication tools, Adobe Suite, Canva, and DAX.</li>
+                    <li><strong>Strategic Innovation:</strong> Employed Agile methodologies and facilitated Agile sprints to drive projects, enhancing project success and go-to-market effectiveness across industrial platforms. Collaborated with cross-functional teams to align corporate strategies with stakeholder needs, enabling rapid ideation and commercialization.</li>
                     <li><strong>Financial Management:</strong> Drove the financial performance of a $90MM portfolio with data-driven strategies, utilizing multiple CRM tools (e.g., SAP, Salesforce Reports, Salesforce Dashboard, DAX).</li>
                     <li><strong>Strategy:</strong> Utilized Power BI to analyze consumer behavior and sales trends from consumer reports, transforming raw data into actionable insights for strategic direction.</li>
                 </ul>
@@ -84,6 +86,10 @@ document.addEventListener('DOMContentLoaded', function() {
         description.innerHTML = item.description; // Use innerHTML to insert the list
         description.style.display = 'none'; // Initially hide the description
 
+        let location = document.createElement('h4'); // Change from <p> to <div> for proper formatting
+        location.innerHTML = item.location; // Use innerHTML to insert the list
+        location.style.display = item.location; // Initially hide the description
+
         // Click event to toggle the description visibility
         entry.addEventListener('click', function() {
             description.style.display = description.style.display === 'none' ? 'block' : 'none';
@@ -91,7 +97,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
         content.appendChild(title);
         content.appendChild(companyName);
+        content.appendChild(location);
         content.appendChild(description);
+        
 
         entry.appendChild(dot);
         entry.appendChild(date);
