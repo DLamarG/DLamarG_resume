0121c95e8372cd598af84f08fd4da71b19a4212f
DLamarG <demondg8@gmail.com>
2024-11-18 22:02:21 -0600

    updated my job history

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-
 document.addEventListener('DOMContentLoaded', function() {
+    // Skills Section: Expand/Collapse Descriptions
     const skillTitles = document.querySelectorAll('.skills-title');
 
     skillTitles.forEach(title => {
@@ -8,50 +8,64 @@ document.addEventListener('DOMContentLoaded', function() {
             description.style.display = description.style.display === 'none' ? 'block' : 'none'; // Toggle visibility
         });
     });
-});
 
-
-document.addEventListener('DOMContentLoaded', function() {
+    // Experience Section: Populate Timeline
     const timelineData = [
         {
-            date: '08/2021 - Present',
-            title: 'IT Consultant',
-            company: 'Code-Fit',
+            date: '05/2022 - 10/2024',
+            title: 'Jr. Cloud Engineer',
+            company: 'Miranda Healthcare',
             location: 'Flossmoor, IL, USA',
             description: `
                 <ul>
-                    <li><strong>System Hardening:</strong> Update and install patches with Windows Update for Windows systems and apt for Ubuntu/Debian systems.</li>
-                    <li><strong>Network Security:</strong> Configure pfSense firewalls, establish network rules with iptables, configure SecurityOnion, and perform pcap analysis with Wireshark to safeguard networks and attached assets.</li>
-                    <li><strong>Cloud Services:</strong> Implement AWS services such as CloudWatch, Route 53, and S3 buckets to manage, monitor, and secure scalable application solutions.</li>
-                    <li><strong>Data Migration:</strong> Executed the migration of databases and file storage to cloud solutions leveraging AWS-S3, optimizing data management processes and ensuring data integrity and accessibility during the transition.</li>
-                    <li><strong>Cloud Security Monotoring:</strong> Implemented monitoring solutions by levaraging AWS CloudWatch, CloudFront, and AWS Sheild to track application performance and resource utilization, enabling proactive adjustments and tuning for optimal operational efficiency.</li>
+                    <li>Designed and deployed full-stack lead/recruitment application leveraging EC2, RDS, S3, Lambda, DynamoDB, and Amplify.</li>
+                    <li>Delivered secure remote desktop solutions using WorkSpaces, WorkLink, IAM, S3, and Amplify.</li>
+                    <li>Designed an analytics pipeline using Redshift, QuickSight, Glue, and Data Pipeline for real-time staffing metrics.</li>
+                    <li>Managed IaC leveraging Terraform and automated deployment/testing with Jenkins or GitHub Actions.</li>
+                    <li>Developed a project management platform utilizing WorkDocs, WorkMail, DynamoDB, AppStream, and SNS.</li>
+                    <li>Utilized CloudWatch for monitoring VPC resources and Grafana for performance dashboards.</li>
                 </ul>
             `
         },
         {
-            date: '11/2018 - 04/2019',
+            date: '12/2019 - 04/2022',
+            title: 'IT Project Manager',
+            company: 'Minor-Morris Ltd.',
+            location: 'Joliet, IL, USA',
+            description: `
+                <ul>
+                    <li>Developed a detailed 2-year cloud migration roadmap for all service areas.</li>
+                    <li>Worked with APN solution architects to design cloud architecture while maintaining compliance.</li>
+                    <li>Provided guidance on leveraging AWS services like EC2, EKS, S3, RDS, and Lambda.</li>
+                    <li>Migrated legacy systems to Kubernetes-managed Dockerized microservices.</li>
+                    <li>Implemented IoT-enabled vehicle tracking tools using AWS IoT Core and S3.</li>
+                    <li>Ensured compliance with AWS IAM, KMS, and CloudTrail for secure role-based access control.</li>
+                </ul>
+            `
+        },
+        {
+            date: '11/2018 - 12/2019',
             title: 'Brand Manager',
             company: 'Rust-Oleum',
             location: 'Vernon Hills, IL, USA',
             description: `
                 <ul>
-                    <li><strong>Strategic Innovation:</strong> Employed Agile methodologies and facilitated Agile sprints to drive projects, enhancing project success and go-to-market effectiveness across industrial platforms. Collaborated with cross-functional teams to align corporate strategies with stakeholder needs, enabling rapid ideation and commercialization.</li>
-                    <li><strong>Financial Management:</strong> Drove the financial performance of a $90MM portfolio with data-driven strategies, utilizing multiple CRM tools (e.g., SAP, Salesforce Reports, Salesforce Dashboard, DAX).</li>
-                    <li><strong>Strategy:</strong> Utilized Power BI to analyze consumer behavior and sales trends from consumer reports, transforming raw data into actionable insights for strategic direction.</li>
+                    <li>Led initiatives to foster innovation across industrial platforms using Agile methodologies.</li>
+                    <li>Drove financial performance of a $90MM portfolio using data-driven strategies.</li>
+                    <li>Analyzed consumer behavior with Power BI, transforming data into actionable insights.</li>
                 </ul>
             `
         },
         {
             date: '11/2015 - 10/2018',
-            title: 'Product Manager',
+            title: 'Technical Product Manager',
             company: 'Fuchs',
             location: 'Harvey, IL, USA',
             description: `
                 <ul>
-                    <li><strong>Product Management:</strong> Managed the P&L for a product portfolio with 225 SKUs, driving $30MM revenue every year using analytical tools (e.g., ATD, SAP, Salesforce Analytics).</li>
-                    <li><strong>Communication Materials:</strong> Developed communication materials to educate customers and sales teams on the value-add positioning for the 225 SKUs in the Industrial portfolio.</li>
-                    <li><strong>Sales Tools Update:</strong> Updated sales tools with the latest communication technologies (e.g., Adobe Suite, Canva).</li>
-                    <li><strong>Customer Engagement:</strong> Visited customer facilities for product presentations and collected relevant market data (Travel 25-30%).</li>
+                    <li>Managed P&L for a 225-SKU product portfolio generating $30MM annual revenue.</li>
+                    <li>Developed communication materials to educate customers and sales teams.</li>
+                    <li>Visited customer facilities for product presentations and market data collection.</li>
                 </ul>
             `
         }
@@ -60,46 +74,44 @@ document.addEventListener('DOMContentLoaded', function() {
     const timeline = document.getElementById('timeline');
 
     timelineData.forEach((item, index) => {
-        let entry = document.createElement('div');
+        const entry = document.createElement('div');
         entry.classList.add('timeline-entry');
         entry.setAttribute('id', 'entry-' + index);
 
-        let dot = document.createElement('div');
+        const dot = document.createElement('div');
         dot.classList.add('timeline-dot');
 
-        let date = document.createElement('div');
+        const date = document.createElement('div');
         date.textContent = item.date;
         date.classList.add('timeline-date');
 
-        let content = document.createElement('div');
+        const content = document.createElement('div');
         content.classList.add('timeline-content');
         content.setAttribute('id', 'content-' + index);
 
-        let title = document.createElement('h3');
+        const title = document.createElement('h3');
         title.textContent = item.title;
 
-        let companyName = document.createElement('h4'); // Create a header for the company name
-        companyName.textContent = item.company; // Set the company name
-        companyName.classList.add('company-name'); // Add a class for styling (optional)
+        const company = document.createElement('h4');
+        company.textContent = item.company;
+        company.classList.add('company-name');
 
-        let description = document.createElement('div'); // Change from <p> to <div> for proper formatting
-        description.innerHTML = item.description; // Use innerHTML to insert the list
-        description.style.display = 'none'; // Initially hide the description
+        const location = document.createElement('h4');
+        location.textContent = item.location;
+        location.classList.add('location');
 
-        let location = document.createElement('h4'); // Change from <p> to <div> for proper formatting
-        location.innerHTML = item.location; // Use innerHTML to insert the list
-        location.style.display = item.location; // Initially hide the description
+        const description = document.createElement('div');
+        description.innerHTML = item.description;
+        description.style.display = 'none';
 
-        // Click event to toggle the description visibility
         entry.addEventListener('click', function() {
             description.style.display = description.style.display === 'none' ? 'block' : 'none';
         });
 
         content.appendChild(title);
-        content.appendChild(companyName);
+        content.appendChild(company);
         content.appendChild(location);
         content.appendChild(description);
-        
 
         entry.appendChild(dot);
         entry.appendChild(date);
@@ -108,3 +120,4 @@ document.addEventListener('DOMContentLoaded', function() {
         timeline.appendChild(entry);
     });
 });
+``
